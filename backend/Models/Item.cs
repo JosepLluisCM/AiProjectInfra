5d48fe2d33762994d8d77b16d08be0967bafa3f0
JosepLluisCM <webdevJosep@gmail.com>
2024-09-22 16:57:48 +0200

    Remove unwanted folders from tracking

--- backend/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.Models
-{
-    public class Item
-    {
-        public int Id { get; set; }
-        public string Description { get; set; } = string.Empty;
-        public bool Checked { get; set; } = false;
-    }
-}
