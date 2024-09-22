a2ff4e80a5f2a1062b03d59de1b24f5db82e97f6
JosepLluisCM <webdevJosep@gmail.com>
2024-09-22 16:41:19 +0200

    redo backend project

---
+++ backend/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public bool Checked { get; set; } = false;
+    }
+}
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3d764c3e8e2bd1be7081e2f6b7f897721024b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c23313db30ca4746d84a258a4c2dc95a55bb859")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
